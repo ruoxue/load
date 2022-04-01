331caecbaf309a747b00c3c39134e27870dffbeb
Bob <ruoshuisixue@sina.com>
2022-04-01 00:12:54 -0700

    Add files via upload

--- youtube/src/main/java/AdbUtil.java
+++ youtube/src/main/java/AdbUtil.java
@@ -1,16 +1,33 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 
 public class AdbUtil {
     public String executeCMDconsole(String cmd) {
 
-        System.out.println(":::::::::::::::::::在cmd里面输入" + cmd + "::::::::::::::::::::::");
         Process p;
         String line = null;
+        if (cmd.startsWith("adb")) {
+
+            String osName = System.getProperty("os.name");
+
+
+            if (osName.toLowerCase(Locale.ROOT).indexOf("mac") != -1) {
+                cmd = System.getProperty("user.dir") +
+                        File.separator +
+                        "src" + File.separator +
+                        "main" + File.separator
+                        + "resources" + File.separator + cmd;
+
+            }
+
+        }
+        System.out.println(":::::::::::::::::::在cmd里面输入" + cmd + "::::::::::::::::::::::");
 
         try {
             p = Runtime.getRuntime().exec(cmd);
@@ -114,11 +131,11 @@ public class AdbUtil {
         }
     }
 
-    public boolean keyevent(String key) {
+    public boolean keyevent(int key) {
         return keyevent(getDevices().get(0), key);
     }
 
-    public boolean keyevent(String device, String key) {
+    public boolean keyevent(String device, int key) {
         try {
             executeCMDconsole("adb -s " + device + "  shell  input keyevent  " + key);
             return true;
@@ -141,4 +158,30 @@ public class AdbUtil {
     }
 
 
+    public boolean slide(int x, int y, int x1, int y1) {
+        return slide(getDevices().get(0), x, y, x1, y1);
+    }
+
+
+    public boolean slide(String device, int x, int y, int x1, int y1) {
+        try {
+            executeCMDconsole("adb -s " + device + "  shell swipe   " + x + "  " + y + " " + x1 + "  " + y1);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean longClick(int x, int y) {
+        return longClick(getDevices().get(0), x, y);
+    }
+
+
+    public boolean longClick(String device, int x, int y) {
+        try {
+            return slide(device, x, y, x, y);
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
